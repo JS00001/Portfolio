6cfb8c5eb638f0f25bc3027f4b77b135e70d5e04
JS <49812749+JS00001@users.noreply.github.com>
2022-09-12 20:22:27 -0400

    Update scroll and Project component

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.mdx/,
-      use: [
-        {
-          loader: '@mdx-js/loader',
-          options: {},
-        },
-      ],
-    })
-    return config
-  },
-}
+	reactStrictMode: true,
+};
--- src/components/shared/Project.tsx
+++ src/components/shared/Project.tsx
@@ -1,36 +1,38 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export type ProjectProps = React.HTMLAttributes<HTMLDivElement> & {
-  image: string
-  tag: string
-  title: string
-  url: string
-  description: string
-}
+	image: string;
+	tag: string;
+	title: string;
+	url: string;
+	description: string;
+};
 
 export const Project: React.FC<ProjectProps> = ({
-  image,
-  tag,
-  url,
-  title,
-  description,
-  className = '',
-  ...props
+	image,
+	tag,
+	url,
+	title,
+	description,
+	className = '',
+	...props
 }) => {
-  return (
-    <Link href={url}>
-      <div
-        className={`cursor-pointer rounded-lg p-8 hover:text-primary hover:shadow-md ${className} flex flex-col`}
-        {...props}
-      >
-        <img
-          src={image}
-          className=" max-h-56 rounded-lg border border-gray-200 object-cover"
-        />
-        <p className="pt-6 text-xs font-medium text-gray-500">{tag}</p>
-        <p className="bg-3 text-2xl font-bold">{title}</p>
-        <p className="text-lg leading-relaxed text-gray-500">{description}</p>
-      </div>
-    </Link>
-  )
-}
+	return (
+		<Link href={url}>
+			<div
+				className={`cursor-pointer rounded-lg p-8 hover:text-primary hover:shadow-md ${className} flex flex-col`}
+				{...props}
+			>
+				<div className='h-56 w-full'>
+					<img
+						src={image}
+						className=' h-full w-full rounded-lg border border-gray-200 object-cover'
+					/>
+				</div>
+				<p className='pt-6 text-xs font-medium text-gray-500'>{tag}</p>
+				<p className='bg-3 text-2xl font-bold'>{title}</p>
+				<p className='text-lg leading-relaxed text-gray-500'>{description}</p>
+			</div>
+		</Link>
+	);
+};
