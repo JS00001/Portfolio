c9257b2cfa94a22e5e582e0d67b29c1c99dd120f
JS <49812749+JS00001@users.noreply.github.com>
2023-09-20 17:02:08 -0400

    Add posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PosthogProvider from '../providers/Posthog';
 
 const queryClient = new QueryClient();
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Component {...pageProps} />
-		</QueryClientProvider>
+		<PosthogProvider>
+			<QueryClientProvider client={queryClient}>
+				<Component {...pageProps} />
+			</QueryClientProvider>
+		</PosthogProvider>
 	);
 };
 
